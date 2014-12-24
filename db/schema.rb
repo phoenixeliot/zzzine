2f6399554889177a45a7dc85fda11ca6cad862e4
Cory Etzkorn <coryetzkorn@gmail.com>
2014-12-24 02:03:28 -0600

    Started theme picker

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224062138) do
+ActiveRecord::Schema.define(version: 20141224073014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,23 @@ ActiveRecord::Schema.define(version: 20141224062138) do
   create_table "hashtags", force: true do |t|
     t.string   "title"
     t.integer  "tweet_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "styles", force: true do |t|
+    t.integer  "style_id"
+    t.string   "slug"
+    t.string   "name"
+    t.integer  "order"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "themes", force: true do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.integer  "theme_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Style.create(
+  name: 'Runway',
+  slug: 'runway',
+  style_id: 1,
+  order: 1
+)
+Style.create(
+  name: 'Stark',
+  slug: 'stark',
+  style_id: 2,
+  order: 2
+)
+Style.create(
+  name: 'Neon',
+  slug: 'neon',
+  style_id: 3,
+  order: 3
+)
+Style.create(
+  name: 'Galactic',
+  slug: 'galactic',
+  style_id: 4,
+  order: 4
+)
+Style.create(
+  name: 'Fast Food',
+  slug: 'fastfood',
+  style_id: 5,
+  order: 5
+)
