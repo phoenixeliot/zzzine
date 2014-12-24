bab28ea945546d95d40c676b8b6f4a119ceb624e
Elliott Etzkorn <elliott@messagebus.com>
2014-12-24 16:08:09 -0600

    devise fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :tweets, only: [:index, :show]
   resources :view, only: [:show]
-  root 'tweets#index'
+ 	
+ 	match 'dashboard' => 'tweets#index', :via => [:get],  :as => 'user_root'
+
+	devise_scope :user do
+	  root to: "devise/sessions#new"
+	end
 end
