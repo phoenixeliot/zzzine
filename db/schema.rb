f72e6ebce9a7a00a66f601f1f97f98ce7c2b5587
Elliott Etzkorn <elliott@messagebus.com>
2014-12-24 17:12:26 -0600

    heroku update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224114524) do
+ActiveRecord::Schema.define(version: 20141224212834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20141224114524) do
     t.datetime "updated_at"
   end
 
+  create_table "statuses", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "val"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "styles", force: true do |t|
     t.integer  "style_id"
     t.string   "slug"
@@ -38,14 +45,7 @@ ActiveRecord::Schema.define(version: 20141224114524) do
     t.integer  "order"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "themes", force: true do |t|
-    t.string   "name"
-    t.string   "slug"
-    t.integer  "theme_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "tweets", force: true do |t|
