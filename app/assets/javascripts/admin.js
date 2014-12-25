cfc291952ed6434a4dc402f4c6a39b199dee298f
Cory Etzkorn <coryetzkorn@gmail.com>
2014-12-25 01:32:09 -0600

    Loader working

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
 
-  if($('.theme-picker').length) {
-    $('.theme-option').click(function(e){
-      e.preventDefault();
-      var slug = $(this).data('style-slug');
-      $('body').attr('class', 'theme ' + slug);
-    });
-  }
-
+  /* Random color generator */
   function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
     var color = '#';
@@ -17,6 +10,16 @@ $(document).ready(function() {
     return color;
   }
 
+  /* Flashing buttons */
+  if($('.btn-flash').length) {
+    var signInButton = $('.btn-flash');
+    setInterval(function() {
+      var hex = getRandomColor();
+      signInButton.css({ 'background': hex });
+    }, 600);
+  }
+
+  /* Flashing wordmarks */
   if($('.wordmark').length) {
 
     var letters = $('.wordmark .letter');
@@ -36,39 +39,40 @@ $(document).ready(function() {
       var hex = getRandomColor();
       $(letter).attr({ 'fill': hex });
     }, 100);
-
-
-    var signInButton = $('.btn-large');
-    setInterval(function() {
-      var hex = getRandomColor();
-      signInButton.css({ 'background': hex });
-    }, 600);
-
-    var themes = $('.wordmark .letter');
-    var randLetterIndex;
-    var prevRandLetterIndex;
-
-    setInterval(function() {
-      function setRandLetterIndex() {
-        randLetterIndex = Math.floor(Math.random() * letters.length);
-        if(randLetterIndex === prevRandLetterIndex) {
-          setRandLetterIndex();
-        }
-        prevRandLetterIndex = randLetterIndex;
-      }
-      setRandLetterIndex();
-      var letter = letters[randLetterIndex];
-      var hex = getRandomColor();
-      $(letter).attr({ 'fill': hex });
-    }, 100);
-
-    /*
-    letters.hover(function() {
-      $(this).remove();
-    });
-    */
     
   }
 
+  /* Intermediate processing loader */
+  if($('.loading').length) {
+    var $loading = $('.loading');
+    var $steps = $loading.find('.step');
+    var $progress = $loading.find('.progress');
+    var stepIndex = 0;
+    function updateProgress(status) {
+      $steps.eq(stepIndex).addClass('complete');
+      stepIndex = status;
+      $steps.removeClass('current');
+      if(stepIndex == 0) {
+        $steps.eq(stepIndex).addClass('current');
+      } else if (stepIndex == 1) {
+        $steps.eq(stepIndex).addClass('current');
+        $progress.height('50%');
+      } else if (stepIndex == 2) {
+        $steps.eq(stepIndex).addClass('complete');
+        $progress.height('100%');
+        $('.loading-btn').hide();
+        $('.ready-btn').show();
+      }
+    }
+
+    setInterval(function() {
+      $.when($.getJSON("tweets/" + currentUser.id)).done( function(json) {
+        var status = json['status'];
+        if(status !== stepIndex) {
+          updateProgress(status);
+        }
+      });
+    }, 300);
+  }
 
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  resources :tweets, only: [:index]
+  resources :tweets, only: [:index, :show]
   resources :view, only: [:show]
  	
  	match 'dashboard' => 'tweets#index', :via => [:get],  :as => 'user_root'
