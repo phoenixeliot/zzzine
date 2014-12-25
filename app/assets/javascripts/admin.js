001379d8953dbd427455920742a07d4a7c67e2ef
Cory Etzkorn <coryetzkorn@gmail.com>
2014-12-24 19:52:56 -0600

    Added autoprefixer

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -8,17 +8,17 @@ $(document).ready(function() {
     });
   }
 
-
-  if($('.tweets-index').length) {
-
-    function getRandomColor() {
-      var letters = '0123456789ABCDEF'.split('');
-      var color = '#';
-      for (var i = 0; i < 6; i++ ) {
-          color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+  }
+
+
+  if($('.wordmark').length) {
 
     var letters = $('.wordmark .letter');
     var randLetterIndex;
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def body_class
     controller_name + ' ' + [controller_name, action_name].join('-')
   end
+  def current_controler
+    controller_name
+  end
 end
