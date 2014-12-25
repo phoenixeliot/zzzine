a80f2304fd62ba09eb4fe3ff9b4e2e4fa8a84f0f
Elliott Etzkorn <elliott@messagebus.com>
2014-12-25 05:08:24 -0600

    qx

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", sessions: "custom/sessions"  }
   resources :tweets, only: [:index, :show]
   resources :view, only: [:show]
- 	
+  	
  	match 'new' => 'tweets#index', :via => [:get],  :as => 'user_root'
 
 	devise_scope :user do
