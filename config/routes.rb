bd7e6ad3d1c6931e97479f13c19fa5a4b14459c9
Elliott Etzkorn <elliott@messagebus.com>
2014-12-25 01:43:52 -0600

    devise workaround

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", sessions: "custom/sessions"  }
   resources :tweets, only: [:index, :show]
   resources :view, only: [:show]
  	
  	match 'new' => 'tweets#index', :via => [:get],  :as => 'user_root'
 
 	devise_scope :user do
-	  root to: "devise/sessions#new"
+	  root to: "custom/sessions#new"
 	end
 end
