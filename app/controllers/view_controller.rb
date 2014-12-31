317d9585cdbbabaae4abbe38a40a71bff2d3ae28
Elliott Etzkorn <elliott@messagebus.com>
2014-12-30 20:11:00 -0800

    small improvements

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -7,6 +7,6 @@ class ViewController < ApplicationController
 		@current = User.find_by(uid: params[:id])
 		@current.title, @current.style_id = params[:content], params[:style_id]
 		@current.save
-		render :show
+		redirect_to "/view/#{@current.uid}"
 	end
 end
